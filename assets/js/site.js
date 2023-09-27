5bc4d28acd34be5be9357edbd4f3ccf25114178b
bo-nicolaisen <bni@techcollege.dk>
2023-09-27 10:40:06 +0200

    test

--- assets/js/site.js
+++ assets/js/site.js
@@ -1,1 +1,7 @@
-function
+
+
+var isTouchScreen = 'createTouch' in document || screen.width <= 699 ||
+    navigator.userAgent.match(/(iPhone|iPod|iPad)/) || navigator.userAgent.match(/BlackBerry/) ||
+    navigator.userAgent.match(/Android/);
+
+console.log(isTouchScreen);
